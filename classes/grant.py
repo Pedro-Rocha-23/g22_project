d5a38f8cfe97aa8cb6a896b66dce25f54db7b3d3
Filipa-2007 <up202507908@up.pt>
2026-04-25 08:52:52 +0100

    Add files via upload

---
+++ classes/grant.py
@@ -0,0 +1,54 @@
+"""
+@author: António Brito / Carlos Bragança
+(2025) objective: class Person
+"""
+# Class Grant - generic version with inheritance
+from classes.gclass import Gclass
+class Grant(Gclass):
+    obj = dict()
+    lst = list()
+    pos = 0
+    sortkey = ''
+    # Attribute names list, identifier attribute must be the first one and callled 'id'
+    att = ['_id','_title','_category']
+    # Class header title
+    header = 'Grants'
+    # field description for use in, for example, input form
+    des = ['Id','Title','Category']
+    # Constructor: Called when an object is instantiated
+    def __init__(self, id, title, category):
+        super().__init__()
+        # Object attributes
+        id = Grant.get_id(id)
+        self._id = id
+        self._title=title
+        self._category=category
+        # Add the new object to the dictionary of objects
+        Grant.obj[id] = self
+        # Add the id to the list of object ids
+        Grant.lst.append(id)
+    # id property getter method
+    @property
+    def id(self):
+        return self._id
+    @id.setter
+    def id(self, id):
+        self._id = id
+    # title property getter method
+    @property
+    def title(self):
+        return self._title
+    @title.setter
+    def title(self, title):
+        self._title=title
+    # category property getter method
+    @property
+    def category(self):
+        return self._category
+    # category property setter method
+    @category.setter
+    def category(self, category):
+        self._category = category
+   
+
+
