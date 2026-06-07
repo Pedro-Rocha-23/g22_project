a58c5ee7539b15c0f79c011cd107487f717d4352
Pedro-Rocha-23 <202505837@up.pt>
2026-06-07 14:42:34 +0100

    Alteração

--- g22_flask/classes/region.py
+++ g22_flask/classes/region.py
@@ -6,28 +6,27 @@ class Region(Gclass):
     pos = 0
     sortkey = ''
     
-    # Atributos privados, o ID tem de ser o primeiro
     att = ['_id', '_name']
     
-    # Título do cabeçalho
+
     header = 'Regions'
-    
-    # Descrição para formulários
+
     des = ['Id', 'Region Name']
     
-    # Construtor
+  
     def __init__(self, id, name):
         super().__init__()
-        # Usar o método get_id da Gclass (tal como no Director)
+       
+        
         id = Region.get_id(id)
         self._id = id
         self._name = name
         
-        # Adicionar aos dicionários e listas da classe
+
         Region.obj[id] = self
         Region.lst.append(id)
         
-    # Getter e Setter para o ID
+
     @property
     def id(self):
         return self._id
@@ -36,7 +35,7 @@ class Region(Gclass):
     def id(self, id):
         self._id = id
         
-    # Getter e Setter para o Nome
+
     @property
     def name(self):
         return self._name
